5510ebfa78327bf9340ace443e522d5cc728792c
Robert <basehintz@gmail.com>
2020-10-09 13:28:54 -0600

    Basic board styled with opaque circles cut out

--- connect_four/scripts/app.js
+++ connect_four/scripts/app.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+$(() => {
+  const makeTiles = () => {
+    const $divTile = $("<div>").addClass("tile");
+    const $divBoard = $("<div class = board>");
+    $(".board").append($divTile);
+  };
+
+  for (i = 0; i < 42; i++) {
+    console.log(i);
+    makeTiles();
+  }
+});
