9e96ec0fffdc8b51077dc779c2df9201f787824b
Robert <basehintz@gmail.com>
2020-10-13 14:10:50 -0600

    else if incase player tries to make an invalid move

--- connect_four/scripts/app.js
+++ connect_four/scripts/app.js
@@ -1,29 +1,58 @@
 $(() => {
-  let board = new Array(7);
+  let columns = 7;
+  let rows = 6;
+  let board = new Array(columns);
 
   for (i = 0; i < board.length; i++) {
-    board[i] = new Array(6);
+    board[i] = new Array(rows);
     const $colDiv = $("<div>").addClass("column").attr("columnIndex", i);
     $(".board").append($colDiv);
 
-    for (j = 0; j < 6; j++) {
+    for (j = 0; j < rows; j++) {
       board[i][j] = "empty";
       const $rowDiv = $("<div>").addClass("row");
       $colDiv.append($rowDiv);
     }
   }
 
-  $(".column").on("click", function (e) {
+  /////////////////Player move\\\\\\\\\\\\\\\\\\\\\\\\\
+  $(".column").on("click", function Playermove(e) {
     // console.log(board);
     let columnIndex = $(e.currentTarget).attr("columnIndex");
     // console.log(columnIndex);
     let column = board[columnIndex];
-    for (i = column.length - 1; i >= 0; i--) {
+    for (let i = column.length - 1; i >= 0; i--) {
       if (column[i] === "empty") {
-        console.log("it works");
+        // console.log("it works");
         $(e.currentTarget).children().eq(i).css("background-color", "red");
+        column[i] = "red";
+        break;
+      }
+      //if player chooses a full column
+      else if (column[0] !== "empty") {
+        alert("column full, please make a valid move");
+        Playermove();
+      }
+    }
+
+    //check if human won
+
+    ////////////////make AI move\\\\\\\\\\\\\\\\\\\\\\
+    let AIChoice = Math.floor(Math.random() * Math.floor(7));
+    let AIcolumn = board[AIChoice];
+    for (let i = AIcolumn.length - 1; i >= 0; i--) {
+      if (AIcolumn[i] === "empty") {
+        // console.log("it works");
+        $("[columnindex='" + AIChoice + "']")
+          .children()
+          .eq(i)
+          .css("background-color", "black");
+        AIcolumn[i] = "black";
         break;
       }
     }
+
+    // console.log(AIChoice);
+    //check if AI won
   });
 });
